e7c32ea2c64e3de0dd00f32d419b6b7123cce247
nucoz <nuri1419@gmail.com>
2022-02-25 22:18:29 +0300

    db fisrt add

---
+++ DownNotifier/DownNotifierDB/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DownNotifierDB
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime ContolTime { get; set; }
+    }
+}
---
+++ DownNotifier/DownNotifierDB/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+   
+
+   
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
